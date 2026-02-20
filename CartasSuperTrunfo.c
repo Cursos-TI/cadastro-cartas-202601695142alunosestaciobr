631e74bc09ffe196562cc85fb568da8622961324
202601695142alunosestaciobr <202601695142@alunos.estacio.br>
2026-02-20 01:08:54 +0000

    CodigoSuperTrunfo_iniciante_vanderson

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,94 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    
+  /*Definindo as variantes da carta 01*/
+char estado = 'A', cod_carta[4] = "Bra", cidade[20] = "Brasil"; // Use aspas simples para um único caractere
+int populacao = 111, n_pontos_turisticos = 111;
+float area = 1.11, pib = 1.11;
+
+  /*Definindo as variantes da carta 02*/
+char estado_2 = 'A', cod_carta_2[4] = "Bra", cidade_2[20] = "Brasil"; // Use aspas simples para um único caractere
+int populacao_2 = 111, n_pontos_turisticos_2 = 111;
+float area_2 = 1.11, pib_2 = 1.11;
 
   // Área para entrada de dados
 
+printf("**Super Trunfo - Cidades Brasileiras**\n\n");
+printf("CADASTRO CARTA 01\n");
+
+/*Coleta de dados da carta 01*/
+
+printf("DIGITE A LETRA QUE REPRESENTE SEU ESTADO CARTA 01\n");
+printf("A-ALAGOAS >> B-BAHIA >> C-CEARA >> D-PERNAMBUCO\nE-SERGIPE >> F-PARAIBA >> G-PIAUI >> H-MARANHÃO\n");
+scanf(" %c", &estado);
+printf("\n# AGORA VAMOS CADASTRAR AS CARACTERISTICAS ESTADO CARTA 01 #\n");
+printf("DIGITE A LETRA DO ESTADO ESCOLHIDO MAIS UM NÚMERO ALEATÓRIO: \n");
+scanf(" %s", &cod_carta);
+printf("DIGITE NOME DA CIDADE: \n");
+scanf(" %s", &cidade);
+printf("DIGITE A POPULAÇÃO ATUAL SEM PONTO: \n");
+scanf(" %i", &populacao);
+printf("DIGITE AREA DO TERRITORIO DO SEU ESTADO COM PONTO: \n");
+scanf(" %.3f", &area);
+printf("DIGITE O PIB COM PONTO: \n");
+while (getchar() != '\n'); //Limpa tudo que restou do buffer até enter, foi necessário devido erro ao saltar o valor do pib.
+scanf(" %f", &pib);
+printf("DIGITE QUANTIDADE PONTOS TURÍSTICOS SEM PONTO: \n");
+scanf(" %i", &n_pontos_turisticos);
+
+printf("\n**Super Trunfo - Cidades Brasileiras**\n\n");
+printf("CADASTRO CARTA 02\n");
+
+    /*Coleta de dados da carta 02*/
+
+printf("DIGITE A LETRA QUE REPRESENTE SEU ESTADO CARTA 02\n");
+printf("A-ALAGOAS >> B-BAHIA >> C-CEARA >> D-PERNAMBUCO\nE-SERGIPE >> F-PARAIBA >> G-PIAUI >> H-MARANHÃO\n");
+scanf(" %c", &estado_2);
+printf("\n# AGORA VAMOS CADASTRAR AS CARACTERISTICA ESTADO CARTA 02 #\n");
+printf("DIGITE A LETRA DO ESTADO ESCOLHIDO MAIS UM NÚMERO ALEATÓRIO: \n");
+scanf(" %s", &cod_carta_2);
+printf("DIGITE NOME DA CIDADE: \n");
+scanf(" %s", &cidade_2);
+printf("DIGITE A POPULAÇÃO ATUAL SEM PONTO: \n");
+scanf(" %i", &populacao_2);
+printf("DIGITE AREA DO TERRITORIO DO SEU ESTADO COM PONTO: \n");
+scanf(" %.3f", &area_2);
+while (getchar() != '\n'); //Limpa tudo que restou do buffer até enter, foi necessário devido erro ao saltar o valor do pib.
+printf("DIGITE O PIB COM PONTO: \n");
+scanf(" %f", &pib_2);
+printf("DIGITE QUANTIDADE PONTOS TURÍSTICOS SEM PONTO: \n");
+scanf(" %i", &n_pontos_turisticos_2);
+
   // Área para exibição dos dados da cidade
 
+  /*Trecho do código de visualização do resultado do cadastro carta 01*/
+
+printf("**\n\nSuper Trunfo - Cidades Brasileiras**\n");
+printf("__CARTAS__\n\n");
+
+printf(">> CARTA: 01 << \n");
+printf("ESTADO: %c \n", estado);
+printf("CARTA: %s \n", cod_carta);
+printf("NOME DO ESTADO: %s \n", cidade);
+printf("POPULAÇÃO: %i \n", populacao);
+printf("ÁREA: %.3f \n", area);
+printf("PIB ATUAL: %.3f \n", pib);
+printf("PONTOS TURISTÍCOS: %i \n", n_pontos_turisticos);
+
+    /*Trecho do código de visualização do resultado do cadastro carta 02*/
+    
+printf(" \n \n>> CARTA: 02 << \n");
+printf("ESTADO: %c \n", estado_2);
+printf("CARTA: %s \n", cod_carta_2);
+printf("NOME DO ESTADO: %s \n", cidade_2);
+printf("POPULAÇÃO: %i \n", populacao_2);
+printf("ÁREA: %.3f \n", area_2);
+printf("PIB ATUAL: %.3f \n", pib_2);
+printf("PONTOS TURISTÍCOS: %i \n", n_pontos_turisticos_2);  
+
 return 0;
+
 } 
