eb57a5098fa318a1bcedd6eb83d7233304606b5e
202601695142alunosestaciobr <202601695142@alunos.estacio.br>
2026-02-24 00:11:05 +0000

    Nivel Mestre Vanderson

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,13 +8,15 @@ int main(){
 
     /*Definindo as variantes da carta 01*/
     char estado = 'A', cod_carta[4] = "Bra", cidade[20] = "Brasil"; // Use aspas simples para um único caractere
-    int populacao = 111, n_pontos_turisticos = 111;
-    float area = 1.110, pib = 1.110, densidade_populacional = 1.110, pib_percapita = 1.110;
+    int n_pontos_turisticos = 111, comparacao_cartas = 0;
+    unsigned long int populacao = 111;
+    float area = 1.110, pib = 1.110, densidade_populacional = 1.110, pib_percapita = 1.110, super_poder_cart1 = 1.110;
 
     /*Definindo as variantes da carta 02*/
     char estado_2 = 'A', cod_carta_2[4] = "Bra", cidade_2[20] = "Brasil"; // Use aspas simples para um único caractere
-    int populacao_2 = 111, n_pontos_turisticos_2 = 111;
-    float area_2 = 1.11, pib_2 = 1.11, densidade_populacional_2 = 1.110, pib_percapita_2 = 1.110;
+    int n_pontos_turisticos_2 = 111, comparacao_cartas_2 = 0;;
+    unsigned long int populacao_2 = 111;
+    float area_2 = 1.11, pib_2 = 1.11, densidade_populacional_2 = 1.110, pib_percapita_2 = 1.110, super_poder_cart2 = 1.110;
 
     printf("**Super Trunfo - Cidades Brasileiras**\n\n");
     printf("CADASTRO CARTA 01\n");
@@ -30,7 +32,7 @@ int main(){
     printf("DIGITE NOME DA CIDADE: \n");
     scanf(" %s", &cidade);
     printf("DIGITE A POPULAÇÃO ATUAL SEM PONTO: \n");
-    scanf(" %i", &populacao);
+    scanf(" %lu", &populacao);
     printf("DIGITE AREA DO TERRITORIO DO SEU ESTADO COM PONTO: \n");
     while (getchar() != '\n'); //Limpa tudo que restou do buffer até enter, foi necessário devido erro ao saltar o valor do pib.
     scanf(" %f", &area);
@@ -40,7 +42,7 @@ int main(){
     printf("DIGITE QUANTIDADE PONTOS TURÍSTICOS SEM PONTO: \n");
     scanf(" %i", &n_pontos_turisticos);
 
-    printf("\n**Super Trunfo - Cidades Brasileiras**\n\n");
+    printf("\n\n**Super Trunfo - Cidades Brasileiras**\n\n");
     printf("CADASTRO CARTA 02\n");
 
     /*Coleta de dados da carta 02*/
@@ -54,7 +56,7 @@ int main(){
     printf("DIGITE NOME DA CIDADE: \n");
     scanf(" %s", &cidade_2);
     printf("DIGITE A POPULAÇÃO ATUAL SEM PONTO: \n");
-    scanf(" %i", &populacao_2);
+    scanf(" %lu", &populacao_2);
     printf("DIGITE AREA DO TERRITORIO DO SEU ESTADO COM PONTO: \n");
     while (getchar() != '\n'); //Limpa tudo que restou do buffer até enter, foi necessário devido erro ao saltar o valor do pib.    
     scanf(" %f", &area_2);
@@ -74,21 +76,35 @@ int main(){
     pib_percapita = (float) pib/populacao;//Carta1
     pib_percapita_2 = (float) pib_2/populacao_2;//Carta2
 
+    /*Trecho do código para calcular o Super Poder da Carta1*/
+
+    super_poder_cart1 = (1/densidade_populacional)+(populacao+area+pib+n_pontos_turisticos+pib_percapita);
+
+    /*Trecho do código para calcular o Super Poder da Carta2*/
+
+    super_poder_cart2 = (1/densidade_populacional_2)+(populacao_2+area_2+pib_2+n_pontos_turisticos_2+pib_percapita_2);
+
+    /*Trecho do código para comparação do Super Poder*/
+
+    comparacao_cartas = super_poder_cart1 > super_poder_cart2;
+    comparacao_cartas_2 = super_poder_cart2 > super_poder_cart1;
+
     /*Trecho do código de visualização do resultado do cadastro carta 01*/
 
-    printf("**\n\nSuper Trunfo - Cidades Brasileiras**\n");
+    printf("\n\n**Super Trunfo - Cidades Brasileiras**\n");
     printf("__CARTAS__\n\n");
 
     printf(">> CARTA: 01 << \n");
     printf("ESTADO: %c \n", estado);
     printf("CARTA: %s \n", cod_carta);
     printf("NOME DO ESTADO: %s \n", cidade);
-    printf("POPULAÇÃO: %i \n", populacao);
+    printf("POPULAÇÃO: %lu \n", populacao);
     printf("ÁREA: %.2f km²\n", area);
     printf("PIB ATUAL: %.2f \n", pib);
     printf("PONTOS TURISTÍCOS: %i \n", n_pontos_turisticos);
     printf("DENSIDADE POPULACIONAL: %.2f hab/km²\n", densidade_populacional);
     printf("PIB per Capita: %.2f reais\n", pib_percapita);
+    printf("SUPER PODER: %.2f\n", super_poder_cart1);
 
     /*Trecho do código de visualização do resultado do cadastro carta 02*/
     
@@ -96,12 +112,18 @@ int main(){
     printf("ESTADO: %c \n", estado_2);
     printf("CARTA: %s \n", cod_carta_2);
     printf("NOME DO ESTADO: %s \n", cidade_2);
-    printf("POPULAÇÃO: %i \n", populacao_2);
+    printf("POPULAÇÃO: %lu \n", populacao_2);
     printf("ÁREA: %.2f \n", area_2);
     printf("PIB ATUAL: %.2f \n", pib_2);
     printf("PONTOS TURISTÍCOS: %i \n", n_pontos_turisticos_2);
     printf("DENSIDADE POPULACIONAL: %.2f hab/km²\n", densidade_populacional_2);
     printf("PIB per Capita: %.2f reais\n", pib_percapita_2);
+    printf("SUPER PODER: %.2f\n\n", super_poder_cart2);
+
+    printf("**Super Trunfo - Cidades Brasileiras**\n\n");
+
+    printf("Vencedor Carta1: %i\n", comparacao_cartas);
+    printf("Vencedor Carta2: %i\n\n", comparacao_cartas_2);
 
     return 0;
 }
